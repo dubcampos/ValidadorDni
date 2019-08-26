d6b4ba2b13ccab60e7f312fd43a7de5f6305e13c
dubcampos <duber_cch@hotmail.com>
2019-08-26 12:33:21 -0500

    avance

--- app/src/main/java/com/example/tecsup/validadordni/MainActivity.java
+++ app/src/main/java/com/example/tecsup/validadordni/MainActivity.java
@@ -3,58 +3,72 @@ package com.example.tecsup.validadordni;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
-Button btn;
-TextView resultado;
+private Button btn_consultar;
+private TextView txt_respuesta;
+private String resultado;
+private EditText txt_dni;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = findViewById(R.id.btn_consultar);
-        resultado = findViewById(R.id.txt_respuesta);
+
+        btn_consultar = findViewById(R.id.btn_consultar);
+        txt_respuesta = (TextView)findViewById(R.id.txt_respuesta);
+        txt_dni = (EditText)findViewById(R.id.txt_dni);
     }
 
-    public String FuncionDni(String dni){
-
+    public String ValidarDni(String dni){
+        try{
+            URL url = new URL("http://aplicaciones007.jne.gob.pe/srop_publico/Consulta/Afiliado/GetNombresCiudadano?DNI="+dni);
+            URLConnection con = null;
+            con = url.openConnection();
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            BufferedReader r = new BufferedReader(new InputStreamReader(con.getInputStream(),"ISO-8859-1"));
+            StringBuilder sb = new StringBuilder();
             try{
-                URL url = new URL("http://aplicaciones007.jne.gon.pe/srop_publico/Consulta/Afiliado/GetNombresCiudadano?DNI=2912");
-                URLConnection con =null;
-                con = url.openConnection();
-                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-                StrictMode.setThreadPolicy(policy);
-                BufferedReader r = new BufferedReader(new InputStreamReader(con.getInputStream(), "ISO-8859-1"));
-                StringBuilder sb = new StringBuilder();
-                try{
-                    String s;
-                    while ((s = r.readLine()) != null){
-                        sb.append(s);
-                        sb.append("\n");
-                    }
-                } finally {
-                    r.close();
+                String s;
+                while ((s = r.readLine())!=null){
+                    sb.append(s);
+                    sb.append("\n");
                 }
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+            } finally {
+                r.close();
             }
-         resultado = sb.toString();
-            return resultado;
-         catch (IOException ex){
+            resultado = sb.toString();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+        return resultado;
     }
+
+
+    public void OnClick (View view){
+        ValidarDni(txt_dni.getText().toString());
+        txt_respuesta.setText(resultado);
+    }
+
 
 
 }
